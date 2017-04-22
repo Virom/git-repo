e1a8b4b78db5a62507fa6719be68ecdbb8a1d746
Virom <viromtrans@gmail.com>
2017-04-23 02:24:02 +0300

    Dell

--- vira.go
+++ vira.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
     fmt.Printf("Hello, Roman Smirnov and Vira Smirnova")
+<<<<<<< HEAD
     fmt.Printf("   Hello, Roman")
+=======
+>>>>>>> parent of a217e13... Revert "забрав строку"
 }
