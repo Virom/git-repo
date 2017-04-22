d0cce8ee56f6fd3b1fb70045a96df881adaa7536
Virom <viromtrans@gmail.com>
2017-04-23 02:24:02 +0300

    Revert "Dell"
    This reverts commit e1a8b4b78db5a62507fa6719be68ecdbb8a1d746.

--- vira.go
+++ vira.go
@@ -4,8 +4,5 @@ import "fmt"
 
 func main() {
     fmt.Printf("Hello, Roman Smirnov and Vira Smirnova")
-<<<<<<< HEAD
     fmt.Printf("   Hello, Roman")
-=======
->>>>>>> parent of a217e13... Revert "забрав строку"
 }
